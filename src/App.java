eda3cf9617cbb5455920e9a73860ca89ebe57332
Jhonata Pereira <pereirajhonatan217@gmail.com>
2025-05-20 18:23:25 -0300

    finalizando

--- src/App.java
+++ src/App.java
@@ -1,8 +1,40 @@
+
 import java.util.Scanner;
 
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)  {
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Qual o primeiro numero? ");
+        int numero1 = ler.nextInt();
+
+        System.out.println("Qual o segundo numero? ");
+        int numero2 = ler.nextInt();
+
+        try{
+            contar(numero1, numero2);
+        }catch (ParametrosInvalidosException e){
+            System.out.println("O segundo numero deve ser maior que o primeiro;");
+        }
+    }
+
+static void contar (int numero1, int numero2) throws ParametrosInvalidosException {
+    if (numero1 >= numero2) {
+        throw new ParametrosInvalidosException("O segundo parametro deve ser maior que o primeiro");
+    }
+
+    int contagem = numero2 - numero1;
+
+    for (int i = 1; i <= contagem; i++){
+        System.out.println("Imprimindo o numero "+ i);
     }
 }
+
+}
+
+     
+        
+        
+    
+
